2071b529cde7d3f116665bbfbc3dc8a5184ade82
nextlevelshit <mail@dailysh.it>
2016-01-18 23:42:09 +0100

    Added webview with manipulated css

--- app/head.js
+++ app/head.js
@@ -3,5 +3,11 @@ require('foundation-sites');
 // Should work, but it does not
 // $(document).foundation();
 setTimeout(function(){
-    $(document).foundation()
+    $(document).foundation();
+    var webview = document.getElementById('browser');
+    webview.addEventListener("dom-ready", function(){
+        console.log('View loaded');
+        webview.insertCSS('*:hover { box-shadow: 0 0 10px #2199e8 !important; }');
+    });
 }, 200);
+
