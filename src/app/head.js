2de76253513b4b11b144f3bf530234c05fdd9cf7
nextlevelshit <mail@dailysh.it>
2016-01-21 23:29:55 +0100

    Removed hacky stuff

--- src/app/head.js
+++ src/app/head.js
@@ -1,13 +1,10 @@
 // Getting sure jquery is loaded correctly
-var $ = jquery = jQuery = window.jQuery = window.$ = require('jquery');
+var $ = window.$ = jQuery = window.jQuery = require('jquery');
 // Loaded libraries you need in the frontend
 require('foundation-sites');
 
-// TODO: Remove this unpleasant workaround
-// Instead this should work, but it does not:
-// $(document).foundation();
-setTimeout(function(){
+$(document).ready(function(){
     $(document).foundation();
-}, 500);
+});
 
 
